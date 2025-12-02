6fb88e01a4d7541f9f488cae8ca02fd66802d34b
tmj <mj_t@zju.edu.cn>
2025-12-02 12:13:45 +0800

    update version tags

--- src/rdkit_dof/__init__.py
+++ src/rdkit_dof/__init__.py
@@ -1,11 +1,14 @@
+"""
+Author: TMJ
+Date: 2025-12-01 12:37:38
+LastEditors: TMJ
+LastEditTime: 2025-12-02 12:12:41
+Description: 请填写简介
+"""
+
 from .config import DofDrawSettings, dofconfig
 from .core import MolGridToDofImage, MolToDofImage
 
-__all__ = [
-    "MolGridToDofImage",
-    "MolToDofImage",
-    "DofDrawSettings",
-    "dofconfig"
-]
+__all__ = ["MolGridToDofImage", "MolToDofImage", "DofDrawSettings", "dofconfig"]
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
